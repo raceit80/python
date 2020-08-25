4c2b0fdc42f9d2f471092e2c11c23c2acd9734f3
Subramanian, Ravi <RAVI_SUBRAMANIAN@homedepot.com>
2020-08-25 09:07:29 -0500

    data analysis python

--- hanoi.py
+++ hanoi.py
@@ -1,12 +1,15 @@
-def move(a,b):
-    print(f'Moving a disc from {a} to {b}')
+def move(a,b,i):
+    print(f'{i}. Moving a disc from {a} to {b}')
+ 
 
 def hanoi(n, f, h, t):
+    iterator = 0
     if n<1:
-        pass
+        return
     else:
         hanoi(n-1, f, t, h)
-        move(f, t)
+        move(f, t, iterator)
+        iterator+=1
         hanoi(n-1, h, f, t)
 
 hanoi(5, 'A', 'B', 'C')
